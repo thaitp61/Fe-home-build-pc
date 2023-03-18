b6e680f394b2b6c0eb3005126898373920069fb0
thanhvp <phuocthanh20cgh@gmail.com>
2023-03-18 14:52:20 +0700

    upload

--- src/components/AppAbout.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class AppAbout extends Component {
-  render() {
-    return (
-      <div>AppAbout</div>
-    )
-  }
-}
--- src/components/AppContact.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-export default class AppContact extends Component {
-  render() {
-    return (
-      <div>AppContact</div>
-    )
-  }
-}
